3a6e338b9a06b9437d94fb07b12765aad63fbfbd
kingaranjoroge <briankingara4@gmail.com>
2022-12-31 19:19:44 +0300

    first commit

---
+++ src/context/DataContext.js
@@ -0,0 +1,57 @@
+import { createContext, useState, useEffect } from 'react'
+import useAxiosFetch from '../hooks/useAxiosFetch'
+
+const DataContext = createContext({})
+
+export const DataProvider = ({ children }) => {
+
+  const [posts, setPosts] = useState([])
+  const [search, setSearch] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+
+  /*useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch (err) {
+        if (err.response) {
+          // Not in the 200 response range 
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    }
+
+    fetchPosts();
+  }, []) */
+
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts')
+
+  useEffect(() => {
+    setPosts(data)
+  }, [data])
+
+  useEffect(() => {
+    const filteredResults = posts.filter((post) =>
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search])
+
+    return (
+        <DataContext.Provider value={{
+            search, setSearch, 
+            searchResults, fetchError, isLoading, 
+            posts, setPosts
+        }}>
+            {children}
+        </DataContext.Provider>
+    )
+}
+
+export default DataContext
